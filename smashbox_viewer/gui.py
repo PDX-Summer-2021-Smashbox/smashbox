bd97f6280e399293deedd5337308c76a6a572af5
Michael Bottini <mbottini1@gmail.com>
2021-07-10 17:03:44 -0700

    Make resource imports portable
    To deploy this project, we're building a .whl here so that you can install the
    whole shebang with pip. What this means in practice is that you can't specify
    filepaths of PNGs - we could be running smashbox_viewer.gui from anywhere. Not
    only that, the package (with the PNGs inside it) could be anywhere - the global
    Python installation, the user's own Python installation, or a venv.
    
    As a result, there's a layer of abstraction that has to be done, and there's a
    library importlib.resources that allows us to grab resources. There is now
    a wrapper function that returns a context manager of these resources.
    
    I also added Pillow to the list of dependencies, so attempting to install the
    wheel will also install Pillow.

--- smashbox_viewer/gui.py
+++ smashbox_viewer/gui.py
@@ -1,9 +1,21 @@
+import importlib.resources
+import tkinter as tk
+
+from PIL import ImageTk, Image
+import pygame
+
 from smashbox_viewer.event_gen import EventGenerator
 from smashbox_viewer.button_locations import BUTTON_LOCATIONS
 from smashbox_viewer.poller import *
-import tkinter as tk
-from PIL import ImageTk, Image
-import pygame
+import smashbox_viewer.resources.skins.default as resources
+
+
+def get_resource(filename):
+    """
+    Wrapper around importlib's resources functionality. Returns a context
+    manager that represents a filehandle.
+    """
+    return importlib.resources.path(resources, filename)
 
 
 class Gui:
@@ -14,25 +26,30 @@ class Gui:
 
     TODO - Once mapping is done button images need to be properly
     choosen and assigned with button locations. Buttons will probably
-    need a data structure to track location, button image, pressed image. 
+    need a data structure to track location, button image, pressed image.
     """
+
     def __init__(self, master):
         self.master = master
         self.buttons = []
-    
+
         master.title("Smashbox Viewer")
         self.bt_frame = tk.Frame(master=self.master)
         self.bt_frame.pack()
         self.canvas = tk.Canvas(self.bt_frame, width=1219, height=624)
         self.canvas.pack()
 
-        self.background = ImageTk.PhotoImage(
-            Image.open("resources/skins/default/base-unmapped.png")
-        )
-        self.button = ImageTk.PhotoImage(Image.open("resources/skins/default/A-2.png"))
-        self.pressed = ImageTk.PhotoImage(Image.open("resources/skins/default/A.png"))
+        with get_resource("base-unmapped.png") as img_fh:
+            self.background = ImageTk.PhotoImage(Image.open(img_fh))
+
+        with get_resource("A-2.png") as img_fh:
+            self.button = ImageTk.PhotoImage(Image.open(img_fh))
+
+        with get_resource("A.png") as img_fh:
+            self.pressed = ImageTk.PhotoImage(Image.open(img_fh))
+
         self.canvas.create_image(0, 0, anchor="nw", image=self.background)
-        
+
         for bt in BUTTON_LOCATIONS:
             self.buttons.append(
                 self.canvas.create_image(
@@ -43,28 +60,31 @@ class Gui:
         # Joystick / trigger drawing for debuging
         self.boxes = [
             # Joystick boxes
-            self.canvas.create_rectangle(200,300,300,400,outline='black'),
-            self.canvas.create_rectangle(200,400,300,500,outline='black'),
+            self.canvas.create_rectangle(200, 300, 300, 400, outline="black"),
+            self.canvas.create_rectangle(200, 400, 300, 500, outline="black"),
             # Trigger Single axis
-            self.canvas.create_rectangle(350,300,375,400,outline='black'),
-            self.canvas.create_rectangle(350,400,375,500,outline='black')
+            self.canvas.create_rectangle(350, 300, 375, 400, outline="black"),
+            self.canvas.create_rectangle(350, 400, 375, 500, outline="black"),
         ]
         # Stick one center locations, and oval representaion
         self.stick1 = [
-            245, 345,
-            self.canvas.create_oval(245,345,255,355,fill='grey',outline='black'),
+            245,
+            345,
+            self.canvas.create_oval(245, 345, 255, 355, fill="grey", outline="black"),
         ]
         # Stick two center locations, and oval
         self.stick2 = [
-            245, 445,
-            self.canvas.create_oval(245,445,255,455,fill='yellow',outline='black'),
+            245,
+            445,
+            self.canvas.create_oval(245, 445, 255, 455, fill="yellow", outline="black"),
         ]
-        
+
         # Trigger center locations, and rectangle representations
         self.triggers = [
-            350, 450,
-            self.canvas.create_rectangle(350,345,375,355,fill='blue'),
-            self.canvas.create_rectangle(350,445,375,455,fill='blue')
+            350,
+            450,
+            self.canvas.create_rectangle(350, 345, 375, 355, fill="blue"),
+            self.canvas.create_rectangle(350, 445, 375, 455, fill="blue"),
         ]
 
     """
@@ -75,56 +95,56 @@ class Gui:
     TODO: once mapping is handled will need to handle buttons individually
     to get proper images
     """
+
     def update(self, diff):
         if "Button" in diff[0]:
             key = diff[0].split("n")
-            num = int(key[1])+1
+            num = int(key[1]) + 1
             if diff[1] == 1:
-                self.canvas.itemconfig(self.buttons[num],image=self.pressed)
+                self.canvas.itemconfig(self.buttons[num], image=self.pressed)
             else:
-                self.canvas.itemconfig(self.buttons[num],image=self.button)
+                self.canvas.itemconfig(self.buttons[num], image=self.button)
 
-        if 'Axis0' in diff[0]:
+        if "Axis0" in diff[0]:
             loc = self.canvas.coords(self.stick1[2])
             loc[0] = diff[1] * 50 + self.stick1[0]
             loc[2] = loc[0] + 10
-            self.canvas.coords(self.stick1[2],loc)
-        if 'Axis1' in diff[0]:
+            self.canvas.coords(self.stick1[2], loc)
+        if "Axis1" in diff[0]:
             loc = self.canvas.coords(self.stick1[2])
             loc[1] = diff[1] * 50 + self.stick1[1]
             loc[3] = loc[1] + 10
-            self.canvas.coords(self.stick1[2],loc)
+            self.canvas.coords(self.stick1[2], loc)
 
-        if 'Axis5' in diff[0]:
+        if "Axis5" in diff[0]:
             loc = self.canvas.coords(self.stick2[2])
             loc[0] = diff[1] * 50 + self.stick2[0]
             loc[2] = loc[0] + 10
-            self.canvas.coords(self.stick2[2],loc)
-        if 'Axis2' in diff[0]:
+            self.canvas.coords(self.stick2[2], loc)
+        if "Axis2" in diff[0]:
             loc = self.canvas.coords(self.stick2[2])
             loc[1] = diff[1] * 50 + self.stick2[1]
             loc[3] = loc[1] + 10
-            self.canvas.coords(self.stick2[2],loc)
+            self.canvas.coords(self.stick2[2], loc)
 
-        if 'Axis3' in diff[0]:
+        if "Axis3" in diff[0]:
             loc = self.canvas.coords(self.triggers[2])
             loc[1] = diff[1] * 50 + self.triggers[0]
             loc[3] = loc[1] + 10
-            self.canvas.coords(self.triggers[2],loc)
-        if 'Axis4' in diff[0]:
+            self.canvas.coords(self.triggers[2], loc)
+        if "Axis4" in diff[0]:
             loc = self.canvas.coords(self.triggers[3])
             loc[1] = diff[1] * 50 + self.triggers[1]
             loc[3] = loc[1] + 10
-            self.canvas.coords(self.triggers[3],loc)
+            self.canvas.coords(self.triggers[3], loc)
 
         self.master.update()
-        
 
 
-"""
 # Testing program
 
-if __name__ == "__main__":
+
+def main():
     root = tk.Tk()
     my_gui = Gui(root)
 
@@ -138,4 +158,7 @@ if __name__ == "__main__":
     for event in EventGenerator(device):
         my_gui.update(event)
         print(event)
-"""
+
+
+if __name__ == "__main__":
+    main()
