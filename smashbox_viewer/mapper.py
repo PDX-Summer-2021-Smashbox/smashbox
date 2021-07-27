40279ded26b5d9674654623924102f8b5a7ea568
bwpdx2 <56132060+bwpdx2@users.noreply.github.com>
2021-07-26 22:37:59 -0700

    Update mapper.py

--- smashbox_viewer/mapper.py
+++ smashbox_viewer/mapper.py
@@ -1,4 +1,7 @@
+<<<<<<< Updated upstream
 from smashbox_viewer import button_roles
+=======
+>>>>>>> Stashed changes
 from smashbox_viewer.button_roles import BUTTON_ROLES
 from smashbox_viewer.button_locations import BUTTON_NAMES
 
@@ -10,11 +13,18 @@ class Mapper:
 
     The cli will return the mapped button dictionary
 
+<<<<<<< Updated upstream
     TODO  GUI client                             
+=======
+    TODO
+        Clean up code a bit  
+        GUI client                             
+>>>>>>> Stashed changes
     '''
     def __init__(self):
         self.mapped_buttons = {}
 
+<<<<<<< Updated upstream
     
     def cli(self):
         # Print the button roles for the user
@@ -27,6 +37,26 @@ class Mapper:
             while not int(map_index) in range(len(BUTTON_ROLES)):
                 map_index = input("Set {0} to index: ".format(button))
             self.mapped_buttons[button] = BUTTON_ROLES[int(map_index)]
+=======
+    def verify_input(self, button):
+        map_index = -1
+        while not int(map_index) in range(len(BUTTON_ROLES)):
+            index = input("Set {0} to index: ".format(button))
+            map_index = index if index.isdigit() else -1
+
+        return int(map_index)
+
+    
+    def cli(self):
+        print("INDEX    BUTTON ROLE")
+        for indx, role in enumerate(BUTTON_ROLES):
+            print("{0}    |  {1}".format(indx, role))
+        # Loop thru each button and map to a role    
+        for button in BUTTON_NAMES:
+            map_index = self.verify_input(button)
+
+            self.mapped_buttons[button] = BUTTON_ROLES[map_index]
+>>>>>>> Stashed changes
         
         return self.mapped_buttons
 
@@ -38,6 +68,10 @@ class Mapper:
 
 
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 # Testing
 
 def main():
